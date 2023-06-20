d25f4f7d8cc2626a6f7d9aceea4999718f29a307
PooniaPankaj <100115111+PooniaPankaj@users.noreply.github.com>
2023-06-21 00:27:53 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,8 +6,8 @@ import { Link, useNavigate } from "react-router-dom"
 function Login(props) {
     const navigate = useNavigate();
 
-    // const host = "http://localhost:8000";
-    const host = "https://scribble-space-app-api.onrender.com"
+    const host = "http://localhost:8000";
+    // const host = "https://scribble-space-app-api.onrender.com"
     const [credentials, setcredentials] = useState({ email: "", password: "" });
     const handlesubmit = async (e) => {
 
